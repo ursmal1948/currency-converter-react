822b3533fd85ef3183511c4e2a21e52e82533306
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-12 14:08:42 +0100

    array currencies  created  in a seperate file

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { name: "Dolar amerykański", shortName: "USD", id: 1, rate: 0.22, },
+    { name: "Korona norweska", shortName: "NOK", id: 2, rate: 2.19 },
+    { name: "Funt brytyjski", shortName: "GBP", id: 3, rate: 0.18 },
+    { name: "Euro", shortName: "EUR", id: 4, rate: 0.18 },
+]
+
+export {currencies};
