a5ceabc47f16db27e8cc53a53f0d550e29769d7a
Urszula Malinowska <ula.malin35@gmail.com>
2023-02-20 15:05:17 +0100

    clock's formating  and adding styles

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,12 +1,24 @@
 import "./style.css";
 
-
 const Header = ({ title, date }) => (
   <header>
+    <span className="clockContent">
+      Dzisiaj jest&nbsp;
+      {date.toLocaleString("pl-PL",
+        {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        }
+      )}
+    </span>
     <h2 className="header">
       {title}
     </h2>
-    <h3>{date}</h3>
   </header>
 );
 
