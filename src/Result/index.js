11683c901144c5ef01d34aac1b830f969e0f520b
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-03 22:08:15 +0100

    Styled Component Result created

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,18 +1,16 @@
-import "./style.css"
 import React from "react";
+import { FinalResult } from "./styled";
 
-const Result = ({ result }) => (
-
-    !!result && (
-        <React.Fragment>
-            <p className="result">
-                {result.initialAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                <strong>
-                    &nbsp;{result.resultAmount.toFixed(2)} {result.currency}
-                </strong>
-            </p>
-        </React.Fragment>
-    )
-);
+const Result = ({ result }) =>
+  !!result && (
+    <React.Fragment>
+      <FinalResult>
+        {result.initialAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+        <strong>
+          &nbsp;{result.resultAmount.toFixed(2)} {result.currency}
+        </strong>
+      </FinalResult>
+    </React.Fragment>
+  );
 
 export default Result;
---
+++ src/Result/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FinalResult = styled.p`
+  display: flex;
+  align-content: flex-start;
+  justify-content: center;
+  padding-bottom: 10px;
+  font-size: 20px;
+  color: rgb(188, 77, 203);
+`;
