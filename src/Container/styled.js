3e44aa3d30f288ef4b4c7b7069040956950dd69e
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-05 23:17:06 +0100

    theme implemented in Container component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -11,11 +11,14 @@ export const StyledContainer = styled(Container)`
   box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
     rgba(0, 0, 0, 0.22) 0px 15px 12px;
 
-  @media (min-width: 321px) and (max-width: 767px) {
-    max-width: 350px;
+  @media 
+  (min-width: ${({ theme }) => theme.breakpoints.iphoneSE}px) 
+  and 
+  (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+  max-width: 350px;
   }
 
-  @media (max-width: 320px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.iphoneSE}px) {
     max-width: 80%;
     margin-right: 40px;
   }
