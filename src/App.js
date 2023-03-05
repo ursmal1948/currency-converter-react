bb6c0e4a69fee40fe21e4ced42bdcb21d5e28896
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-05 23:15:20 +0100

    theme created and moved to a seperate file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Form from "./Form";
 import Result from "./Result";
-import { currencies } from './currencies';
-import {StyledClock} from "./Clock/styled"
-import { StyledHeader } from './Header/styled';
-import { StyledContainer } from './Container/styled';
+import { currencies } from "./currencies";
+import { StyledClock } from "./Clock/styled";
+import { StyledHeader } from "./Header/styled";
+import { StyledContainer } from "./Container/styled";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 function App() {
-
   const [result, setResult] = useState("");
 
   const calculateResult = (amount, currency) => {
+    const rate = currencies.find(
+      ({ shortName }) => shortName === currency
+    ).rate;
 
-    const rate = currencies.find(({ shortName }) => shortName === currency).rate;
-
-    return (
-      setResult({
-        initialAmount: +amount,
-        resultAmount: amount * rate,
-        currency: currency,
-      }))
+    return setResult({
+      initialAmount: +amount,
+      resultAmount: amount * rate,
+      currency: currency,
+    });
   };
 
   return (
-    <StyledContainer>
-      <StyledClock/>
-      <StyledHeader
-        title="Kalkulator walutowy"
-      />
-      <Form
-        calculateResult={calculateResult}
-      />
-      <Result
-        result={result}
-        calculateResult={calculateResult}
-      />
-    </StyledContainer>
+    <ThemeProvider theme={theme}>
+      <StyledContainer>
+        <StyledClock />
+        <StyledHeader title="Kalkulator walutowy" />
+        <Form calculateResult={calculateResult} />
+        <Result result={result} calculateResult={calculateResult} />
+      </StyledContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primaryColor: "hsl(180, 100%, 30%)",
+    majorColor: "rgb(188, 77, 203)",
+  },
+  breakpoints: {
+    mobile: 767,
+    iphoneSE:320,
+  },
+};
