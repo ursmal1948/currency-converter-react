0dcac7f697140510af1910b2f954381d39e3203b
Urszula Malinowska <ula.malin35@gmail.com>
2023-03-10 13:59:10 +0100

    deleted React Fragment

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,16 +1,13 @@
-import React from "react";
 import { FinalResult } from "./styled";
 
 const Result = ({ result }) =>
   !!result && (
-    <React.Fragment>
       <FinalResult>
         {result.initialAmount.toFixed(2)}&nbsp;PLN&nbsp;=
         <strong>
           &nbsp;{result.resultAmount.toFixed(2)} {result.currency}
         </strong>
       </FinalResult>
-    </React.Fragment>
   );
 
 export default Result;
